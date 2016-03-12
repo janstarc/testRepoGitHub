4b8ffe9eead76e0fd92c10abac8111bb332a44be
js5898 <js5898@student.uni-lj.si>
2016-03-12 14:09:09 +0000

    Dodan feature za branje stevil

--- src/java1.c
+++ src/java1.c
@@ -4,5 +4,12 @@ int main (){
     printf("Prva verzija - 0.1\n");
     printf("Develop 1 --- polepsan izpis\n");
     
+    printf("\n");
+    printf("Feature branch - prvi feature - branje dveh stevil: ");
+    int prva;
+    int druga;
+    scanf("%d", &prva);
+    scanf("%d", &druga);
+    printf("Vneseni stevili: %d in %d", prva, druga);
     return 0;
 }
