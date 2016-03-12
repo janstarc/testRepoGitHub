d0f7432d917c364cc897e20d21cc9727b519a88a
js5898 <js5898@student.uni-lj.si>
2016-03-12 14:03:36 +0000

    Polepsan izpis v branchu Develop

--- src/java1.c
+++ src/java1.c
@@ -1,4 +1,8 @@
 int main (){
+    printf("\n");
+    printf("Develop 1 --- polepsan izpis\n");
     printf("Prva verzija - 0.1\n");
+    printf("Develop 1 --- polepsan izpis\n");
+    
     return 0;
 }
