5e541f1128305715665a7e7470141423a6dff101
js5898 <js5898@student.uni-lj.si>
2016-03-12 14:11:16 +0000

    Druga sprememba v develop branchu, feature branch eno verzijo zadaj

--- src/java1.c
+++ src/java1.c
@@ -2,7 +2,15 @@ int main (){
     printf("\n");
     printf("Develop 1 --- polepsan izpis\n");
     printf("Prva verzija - 0.1\n");
+    printf("Develop2 -- dodatek v drugi verziji commita");
     printf("Develop 1 --- polepsan izpis\n");
     
+    printf("\n");
+    printf("Feature branch - prvi feature - branje dveh stevil: ");
+    int prva;
+    int druga;
+    scanf("%d", &prva);
+    scanf("%d", &druga);
+    printf("Vneseni stevili: %d in %d", prva, druga);
     return 0;
 }
